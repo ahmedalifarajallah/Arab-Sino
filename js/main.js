92f4de7f57f982de23b7b8bbf73cb2c9895e8c17
ahmedalifarajallah <ahmedfarajallah00@gmail.com>
2024-12-02 20:49:31 +0200

    last

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const closeIcon = document.getElementById("custom-close-icon");
 const mobMenu = document.getElementById("products-menu");
 
 openIcon.addEventListener("click", () => {
+  console.log("Asa");
+  
   mobMenu.style.display = "block";
   document.body.style.overflow = "hidden";
   setTimeout(() => {
