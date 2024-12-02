7cb9e4f5a11e059a4f0c628081963eb8ec07c6ec
ahmedalifarajallah <ahmedfarajallah00@gmail.com>
2024-12-02 19:50:27 +0200

    Last Commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const openIcon = document.getElementById("mob-icon");
+const closeIcon = document.getElementById("custom-close-icon");
+const mobMenu = document.getElementById("products-menu");
+
+openIcon.addEventListener("click", () => {
+  mobMenu.style.display = "block";
+  document.body.style.overflow = "hidden";
+  setTimeout(() => {
+    mobMenu.classList.add("open");
+  }, 10);
+});
+
+closeIcon.addEventListener("click", () => {
+  mobMenu.classList.remove("open");
+  setTimeout(() => {
+    mobMenu.style.display = "none";
+    document.body.style.overflow = "";
+  }, 400);
+});
